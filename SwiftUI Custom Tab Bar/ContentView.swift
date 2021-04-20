8f521abd3790461e6beb5121705bdb3d755236fe
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-20 13:50:44 +0000

    Initial Commit

---
+++ SwiftUI Custom Tab Bar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Custom Tab Bar
+//
+//  Created by elhajjaji on 20/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Custom Tab Bar/SwiftUI_Custom_Tab_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Custom_Tab_BarApp.swift
+//  SwiftUI Custom Tab Bar
+//
+//  Created by elhajjaji on 20/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Custom_Tab_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
